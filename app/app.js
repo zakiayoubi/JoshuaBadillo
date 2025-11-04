93b807b877a1c98f01e4877a49725de7b101ba21
Zaki Ayoubi <zakihorakhsh2028@gmail.com>
2025-11-03 20:07:29 -0800

    fix for transporter object

--- app/app.js
+++ app/app.js
@@ -34,14 +34,14 @@ app.post("/submit", async (req, res) => {
     const {name, email, message} = req.body
 
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
-        tls: {
-        rejectUnauthorized: false
-    }
+        tls: { rejectUnauthorized: false }
     });
     
 
